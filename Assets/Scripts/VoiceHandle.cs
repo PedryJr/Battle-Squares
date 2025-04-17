9b70caaad73572210ca563f876a21cdb0f50e310
Pedry <emil.dahl580@gmail.com>
2025-04-17 20:40:23 +0200

    Yoink

--- Assets/Scripts/VoiceHandle.cs
+++ Assets/Scripts/VoiceHandle.cs
@@ -64,7 +64,7 @@ public sealed class VoiceHandle : NetworkBehaviour
         cameraTransform = Camera.main.transform;
 
         if (arg0.name.Equals("MenuScene")) if(gameObject) Destroy(gameObject);
-
+            
     }
 
     private void Update()
