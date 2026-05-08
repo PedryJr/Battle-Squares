6092998f0e9aed7ccc02ed62b03a28100cf1ecf5
HermanPettersson <herman.pettersson93@gmail.com>
2026-05-08 12:12:27 +0200

    Updated UI to add translation script, made change language functions public for use in buttons.

--- Assets/BattleSquares2/Scripts/Translation/GetTranslation.cs
+++ Assets/BattleSquares2/Scripts/Translation/GetTranslation.cs
@@ -18,13 +18,13 @@ public class GetTranslation : MonoBehaviour
 		SetText();
 	}
 	[ContextMenu("Set language to English")]
-	private void ChangeLanguage_0() => Translation_Manager.ChangeLanguage(0);
+	public void ChangeLanguage_0() => Translation_Manager.ChangeLanguage(0);
 	[ContextMenu("Set language to Japanese")]
-	private void ChangeLanguage_1() => Translation_Manager.ChangeLanguage(1);
+	public void ChangeLanguage_1() => Translation_Manager.ChangeLanguage(1);
 	[ContextMenu("Set language to French")]
-	private void ChangeLanguage_2() => Translation_Manager.ChangeLanguage(2);
+    public void ChangeLanguage_2() => Translation_Manager.ChangeLanguage(2);
 	[ContextMenu("Set language to German")]
-	private void ChangeLanguage_3() => Translation_Manager.ChangeLanguage(3);
+    public void ChangeLanguage_3() => Translation_Manager.ChangeLanguage(3);
 	[ContextMenu("Set language to Spanish")]
-	private void ChangeLanguage_4() => Translation_Manager.ChangeLanguage(4);
+    public void ChangeLanguage_4() => Translation_Manager.ChangeLanguage(4);
 }
